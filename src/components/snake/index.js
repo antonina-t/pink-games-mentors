3d896ec260a60dd1b215c86897f827d9968d270e
antonina_n <antonina.nevstedt@consid.se>
2020-08-02 00:16:04 +0200

    Basic webpage

---
+++ src/components/snake/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-container">
+      <h2>Snake game will be here...</h2>
+    </div>
+  );
+}
+
+export default Home;
