f448744600dbcbca29c82be8c1f5b60acfc596c5
antonina_n <antonina.nevstedt@consid.se>
2020-08-06 20:11:26 +0200

    Memory: Add components with some basic functionality

--- src/components/memory/index.js
+++ src/components/memory/index.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import MemoryCard from "./MemoryCard";
+import StatusBar from "./StatusBar";
+import "./index.css";
+
+const colors = [
+  "pink",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "teal",
+  "blue",
+  "purple",
+];
+
+function generateCards() {
+  const cards = [];
+  for (let i = 0; i < colors.length; i++) {
+    cards.push({
+      key: i * 2,
+      color: colors[i],
+      isFlipped: false,
+    });
+    cards.push({
+      key: i * 2 + 1,
+      color: colors[i],
+      isFlipped: false,
+    });
+  }
+  cards.sort(() => Math.random() - 0.5);
+  return cards;
+}
 
 function Memory() {
+  const [cards, setCards] = useState(generateCards());
+  const status = "Time: 0s";
+
+  function onCardClick(key) {
+    setCards((oldCards) =>
+      oldCards.map((card) => {
+        if (card.key === key) return { ...card, isFlipped: true };
+        return card;
+      })
+    );
+  }
+
+  function onRestart() {
+    setCards(generateCards());
+  }
+
   return (
-    <div className="text-container">
-      <h2>Memory game will be here...</h2>
+    <div>
+      <div className="game-container">
+        <StatusBar status={status} onRestart={onRestart}></StatusBar>
+        <div className="memory-grid">
+          {cards.map((card) => (
+            <MemoryCard
+              {...card}
+              onClick={() => onCardClick(card.key)}
+            ></MemoryCard>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
