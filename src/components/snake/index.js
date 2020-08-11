ab3aba407b039d2af580efa9b7a33e85f34acc3c
antonina_n <antonina.nevstedt@consid.se>
2020-08-11 20:12:02 +0200

    Snake lecture

--- src/components/snake/index.js
+++ src/components/snake/index.js
@@ -1,9 +1,109 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
+
+const width = 20;
+const height = 12;
+
+function generateGame() {
+  const snake = {
+    head: { x: width / 2, y: height / 2 },
+    tail: [{ x: width / 2 - 1, y: height / 2 }],
+    dir: "right",
+  };
+  return {
+    snake,
+    food: generateFood(snake),
+  };
+}
+
+function generateFood(snake) {
+  let { x, y } = snake.head;
+  while (
+    isEqual(snake.head, { x, y }) ||
+    snake.tail.some((cell) => isEqual(cell, { x, y }))
+  ) {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+  }
+  return { x, y };
+}
+
+function isEqual(cell1, cell2) {
+  return cell1.x === cell2.x && cell1.y === cell2.y;
+}
+
+function tick(game) {
+  console.log("Updating...");
+  return game;
+}
 
 function Snake() {
+  const [game, setGame] = useState(generateGame());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setGame((oldGame) => tick(oldGame));
+    }, 400);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
+
+  function handleKeyPress(event) {
+    let newDir;
+    switch (event.keyCode) {
+      case 37:
+      case 65:
+        newDir = "left";
+        break;
+      case 38:
+      case 87:
+        newDir = "up";
+        break;
+      case 39:
+      case 68:
+        newDir = "right";
+        break;
+      case 40:
+      case 83:
+        newDir = "down";
+        break;
+    }
+    setGame((oldGame) => {
+      return {
+        ...oldGame,
+        snake: {
+          ...oldGame.snake,
+          dir: newDir,
+        },
+      };
+    });
+  }
+
+  const cells = [];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const cell = { x, y };
+      const className = isEqual(cell, game.snake.head)
+        ? "head"
+        : game.snake.tail.some((tailCell) => isEqual(tailCell, cell))
+        ? "tail"
+        : isEqual(cell, game.food)
+        ? "food"
+        : "";
+
+      cells.push(
+        <div key={`${x}-${y}`} className={"snake-cell " + className}></div>
+      );
+    }
+  }
+
   return (
-    <div className="text-container">
-      <h2>Snake game will be here...</h2>
+    <div className="game-container">
+      <div className="snake-grid">{cells}</div>
     </div>
   );
 }
